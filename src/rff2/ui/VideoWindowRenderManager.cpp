b1af760983304768c2f9acda69bc7f98cfa7e3b4
Merutilm <merutilm@gmail.com>
2026-08-22 13:40:31 +0900

    fix: depth image is not appended to video window

--- src/rff2/ui/VideoWindowRenderManager.cpp
+++ src/rff2/ui/VideoWindowRenderManager.cpp
@@ -121,6 +121,9 @@ namespace merutilm::rff2 {
                           VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT |
                                   VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_SAMPLED_BIT |
                                   VK_IMAGE_USAGE_STORAGE_BIT));
+        sharedImg.appendMultiframeImageContext(
+                MF_MAIN_RENDER_IMAGE_DEPTH,
+                iiiGetter(videoExtent, VK_FORMAT_D32_SFLOAT, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT));
         sharedImg.appendMultiframeImageContext(MF_MAIN_RENDER_DOWNSAMPLED_IMAGE_PRIMARY,
                                                iiiGetter(blurredImageExtent, VK_FORMAT_R8G8B8A8_UNORM,
                                                          VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT |
