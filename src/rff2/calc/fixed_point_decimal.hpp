9a64e77a8abd30636f31c4be247680fa22dbbece
Merutilm <merutilm@gmail.com>
2026-09-12 19:27:35 +0900

    modified gmp source

--- src/rff2/calc/fixed_point_decimal.hpp
+++ src/rff2/calc/fixed_point_decimal.hpp
@@ -193,6 +193,43 @@ namespace merutilm::rff2 {
 
 
     inline void fixed_point_decimal::sqr(fixed_point_decimal &result, const fixed_point_decimal &v) {
+        assert(result.exp2div64 == v.exp2div64);
+
+        /*
+           This function contains modified gmp source code under mpz_mul.
+
+           mpz_mul -- Multiply two integers.
+
+           Copyright 1991, 1993, 1994, 1996, 2000, 2001, 2005, 2009, 2011, 2012,
+           2015 Free Software Foundation, Inc.
+
+           This file is part of the GNU MP Library.
+
+           The GNU MP Library is free software; you can redistribute it and/or modify
+           it under the terms of either:
+
+           * the GNU Lesser General Public License as published by the Free
+             Software Foundation; either version 3 of the License, or (at your
+            option) any later version.
+
+           or
+
+            * the GNU General Public License as published by the Free Software
+             Foundation; either version 2 of the License, or (at your option) any
+             later version.
+
+           or both in parallel, as here.
+
+           The GNU MP Library is distributed in the hope that it will be useful, but
+           WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+           or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+           for more details.
+
+           You should have received copies of the GNU General Public License and the
+           GNU Lesser General Public License along with the GNU MP Library.  If not,
+           see https://www.gnu.org/licenses/.
+           */
+
         const mpz_srcptr l = v.data;
         mp_size_t size = l->_mp_size;
         size = std::abs(size);
@@ -225,17 +262,51 @@ namespace merutilm::rff2 {
         assert(result.exp2div64 == lhs.exp2div64);
         assert(result.exp2div64 == rhs.exp2div64);
 
+        /*
+         *  This function contains modified gmp source code under mpz_mul.
+
+            mpz_mul -- Multiply two integers.
+
+            Copyright 1991, 1993, 1994, 1996, 2000, 2001, 2005, 2009, 2011, 2012,
+            2015 Free Software Foundation, Inc.
+
+            This file is part of the GNU MP Library.
+
+            The GNU MP Library is free software; you can redistribute it and/or modify
+            it under the terms of either:
+
+            * the GNU Lesser General Public License as published by the Free
+              Software Foundation; either version 3 of the License, or (at your
+             option) any later version.
+
+            or
+
+             * the GNU General Public License as published by the Free Software
+              Foundation; either version 2 of the License, or (at your option) any
+              later version.
+
+            or both in parallel, as here.
+
+            The GNU MP Library is distributed in the hope that it will be useful, but
+            WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+            or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+            for more details.
+
+            You should have received copies of the GNU General Public License and the
+            GNU Lesser General Public License along with the GNU MP Library.  If not,
+            see https://www.gnu.org/licenses/.
+            */
 
         mpz_srcptr l = lhs.data;
         mpz_srcptr r = rhs.data;
-        mp_size_t lhs_size = l->_mp_size;
-        mp_size_t rhs_size = r->_mp_size;
+        mp_size_t lhs_size = mpz_size(l);
+        mp_size_t rhs_size = mpz_size(r);
         const mp_size_t sgn = lhs_size ^ rhs_size;
         lhs_size = std::abs(lhs_size);
         rhs_size = std::abs(rhs_size);
         mp_size_t result_size = lhs_size + rhs_size;
 
-        if(lhs_size < rhs_size) {
+        if (lhs_size < rhs_size) {
             std::swap(l, r);
             std::swap(lhs_size, rhs_size);
         }
